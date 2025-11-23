6ad8cf6989ce8b00408daedb40a80ab1c5f4bfaf
Wojciech Król <105241268+xMikuzoo@users.noreply.github.com>
2025-11-23 21:23:47 +0100

    Add Entity Framework Core setup with initial migration and BlogContext

---
+++ lab3/Models/Entities/BlogContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab3.Models.Entities;
+
+public class BlogContext: DbContext
+{
+  public DbSet<BlogArticle> BlogArticles { get; set; }
+  
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlite("Data Source=blog.db");
+  }
+}
